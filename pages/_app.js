d13aaadbd59a0d43321a411ddff6d401e1206d4e
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-03-22 15:52:26 +0100

    feat: add scrollbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import PublicLayout from "@/components/layouts/PublicLayouts";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import '@/styles/scrollbar.css'
+
 
 export default function App({ Component, pageProps }) {
   return (
