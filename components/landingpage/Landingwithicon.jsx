02d5552063e25187412baf6c68d11e7539196d2f
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-03-29 09:36:10 +0100

    fix: breakpoints

--- components/landingpage/Landingwithicon.jsx
+++ components/landingpage/Landingwithicon.jsx
@@ -8,17 +8,18 @@ import {
   TagLabel,
   Text,
   Heading,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import Buttongradient from "./Buttongradient";
 
 export default function Landingwithicon({ iconpath }) {
+  const icontempheight = useBreakpointValue({"2xl":"60vh" ,"3xl": "40vh"})
   return (
     <Box
       pt={16}
       fontFamily={"marianne"}
       color={"white"}
-      bgSize={"200% 100%"}
-      bgPos={"25%"}
+     
       zIndex={0}
     >
       <Grid
@@ -46,8 +47,8 @@ export default function Landingwithicon({ iconpath }) {
           </Heading>
           <Text mt={2} fontSize={"x-large"} >Surveillez et gérez vos fichiers Linux en toute simplicité. Avec une interface conviviale et des fonctionnalités avancées, Nemesis vous offre le contrôle total sur votre système. Sécurisez vos données et optimisez votre environnement Linux dès maintenant</Text>
         </GridItem>
-        <GridItem justifyContent={"center"} ml={20}>
-          <Image src={iconpath} w={"100%"} h={"100%"} alt="logo" boxSize={"75%"}/>
+        <GridItem justifyContent="center" alignItems="center" display="flex" ml={20}>
+          <Image src={iconpath} w={icontempheight} h={icontempheight} alt="logo" />
         </GridItem>
       </Grid>
       <Buttongradient/>
--- utils/chakra-theme.js
+++ utils/chakra-theme.js
@@ -1,5 +1,15 @@
 import { extendTheme } from "@chakra-ui/react"
+const breakpoints = {
+    base: "0px",
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+    "2xl": "1600px",
+    "3xl": "1920px"
+  };
 const theme = extendTheme({
+    breakpoints,
     fonts: {
         heading: "rust",
         body: "rust",
