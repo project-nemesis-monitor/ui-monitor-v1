fca1875f41fd5e48b46144b21848b7920ac2de7f
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-03-30 18:04:48 +0100

    fix: login

--- components/login/Loginpage.jsx
+++ components/login/Loginpage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { LockIcon, UnlockIcon } from '@chakra-ui/icons'
+import { LockIcon, UnlockIcon } from "@chakra-ui/icons";
 import Cookies from "js-cookie";
 import {
   Box,
@@ -16,10 +16,14 @@ import {
   Text,
   InputRightElement,
   IconButton,
-  useDisclosure
+  useDisclosure,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 
 export default function LoginPage() {
+  const formbox = useBreakpointValue({ "2xl": "70vh", "3xl": "50vh" });
+  const padding = useBreakpointValue({ "2xl": "2vh", "3xl": "5vh" });
+  const input = useBreakpointValue({ "2xl": "47vh", "3xl": "40vh" });
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { isOpen, onToggle } = useDisclosure();
@@ -39,25 +43,25 @@ export default function LoginPage() {
       const response = await fetch("/api/checkusr", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           username,
-          password
-        })
+          password,
+        }),
       });
-      console.log(response)
+      console.log(response);
       if (response.ok) {
         const data = await response.json();
-        console.log("data")
-        console.log(data)
-        console.log("data")
+        console.log("data");
+        console.log(data);
+        console.log("data");
         const { token } = data.usr;
 
         Cookies.set("sessionToken", token, {
           secure: true,
           sameSite: "strict",
-          httpOnly: true
+          httpOnly: true,
         });
 
         router.push("/");
@@ -75,7 +79,7 @@ export default function LoginPage() {
         <Image src="/logo1.png" w={"30vh"} alt="Your Image" />
       </Center>
       <Center>
-        <Box bg="#2645F9" borderRadius="md" w={"50vh"} py={"5vh"}>
+        <Box bg="#2645F9" borderRadius="md" w={formbox} py={padding}>
           <chakra.h3
             fontSize="4xl"
             fontWeight="bold"
@@ -91,68 +95,74 @@ export default function LoginPage() {
             expérience numérique inégalée
           </Text>
           <FormControl mx={4} my={4}>
-            <FormLabel color={"white"}>Identifiant</FormLabel>
-            <InputGroup>
-              <InputLeftElement pointerEvents="none">
-                <Image
-                  src={"/icons/user.svg"}
-                  alt="User Icon"
-                  boxSize={7}
-                  pt={0}
-                />
-              </InputLeftElement>
-              <Input
-                type="text"
-                name="username"
-                placeholder="Saisissez votre identifiant"
-                value={username}
-                onChange={handleChange}
-                mb={2}
-                w={"47vh"}
-                bg={"#B2B1B1"}
-              />
-            </InputGroup>
+            <Center>
+              <Box>
+                <FormLabel color={"white"}>Identifiant</FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    <Image
+                      src={"/icons/user.svg"}
+                      alt="User Icon"
+                      boxSize={7}
+                      pt={0}
+                    />
+                  </InputLeftElement>
+                  <Input
+                    type="text"
+                    name="username"
+                    placeholder="Saisissez votre identifiant"
+                    value={username}
+                    onChange={handleChange}
+                    mb={2}
+                    w={input}
+                    bg={"#B2B1B1"}
+                  />
+                </InputGroup>
+              </Box>
+            </Center>
           </FormControl>
           <FormControl mx={4} my={4}>
-            <FormLabel color={"white"}>Mot de passe</FormLabel>
+            <Center>
+              <Box>
+                <FormLabel color={"white"}>Mot de passe</FormLabel>
 
-            <InputGroup>
-              <InputLeftElement pointerEvents="none">
-                <Image
-                  src={"/icons/lock.svg"}
-                  alt="Lock Icon"
-                  boxSize={7}
-                  pt={0}
-                />
-              </InputLeftElement>
-              <Input
-                type={isOpen ? "text" : "password"}
-                name="password"
-                placeholder="Saisissez votre mot de passe"
-                value={password}
-                onChange={handleChange}
-                mb={2}
-                w={"47vh"}
-                bg={"#B2B1B1"}
-              />
-              <InputRightElement>
-                <IconButton
-                  aria-label={
-                    isOpen
-                      ? "Masquer le mot de passe"
-                      : "Afficher le mot de passe"
-                  }
-                  icon={
-                    isOpen ? <UnlockIcon/> : <LockIcon/>
-                  }
-                  variant="ghost"
-                  color={"#2645F9"}
-                  _hover={{}}
-                  ml={-14}
-                  onClick={onToggle}
-                />
-              </InputRightElement>
-            </InputGroup>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    <Image
+                      src={"/icons/lock.svg"}
+                      alt="Lock Icon"
+                      boxSize={7}
+                      pt={0}
+                    />
+                  </InputLeftElement>
+                  <Input
+                    type={isOpen ? "text" : "password"}
+                    name="password"
+                    placeholder="Saisissez votre mot de passe"
+                    value={password}
+                    onChange={handleChange}
+                    mb={2}
+                    w={input}
+                    bg={"#B2B1B1"}
+                  />
+                  <InputRightElement>
+                    <IconButton
+                      aria-label={
+                        isOpen
+                          ? "Masquer le mot de passe"
+                          : "Afficher le mot de passe"
+                      }
+                      icon={isOpen ? <UnlockIcon /> : <LockIcon />}
+                      variant="ghost"
+                      color={"#2645F9"}
+                      _hover={{}}
+                      ml={0}
+                      onClick={onToggle}
+                    />
+                  </InputRightElement>
+                </InputGroup>
+              </Box>
+            </Center>
           </FormControl>
 
           <Center>
