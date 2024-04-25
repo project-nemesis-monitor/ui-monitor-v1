1d63fa58f74b9f3b9aab47d95295fc437e860ee0
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-04-25 23:03:53 +0200

    feat: politique

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -24,7 +24,7 @@ export default function Mentionslegales() {
             <Box fontFamily={"marianne"} color={"white"} p={14}>
 
                 <Text fontFamily={"marianne"} mb={4}>
-                    Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l""Utilisateur", du site www.siteopensourcem1.fr , ci-après le "Site", les présentes mentions légales.
+                    Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l&ldquo;&ldquo;Utilisateur&ldquo;, du site www.siteopensourcem1.fr , ci-après le &ldquo;Site&ldquo;, les présentes mentions légales.
 
                     La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.
 
@@ -33,27 +33,27 @@ export default function Mentionslegales() {
                 </Text>
 
 
-                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 1 - L'EDITEUR</Heading>
+                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 1 - L&apos;EDITEUR</Heading>
                 <Text mb={8} mt={4}>
-                    L’édition et la direction de la publication du Site est assurée par groupe 4 open source, domiciliée 12 rue anatole france, dont le numéro de téléphone est 06000000, et l'adresse e-mail contact@nemesis,com
+                    L’édition et la direction de la publication du Site est assurée par groupe 4 open source, domiciliée 12 rue anatole france, dont le numéro de téléphone est 06000000, et l&apos;adresse e-mail contact@nemesis,com
 
-                    ci-après l'"Editeur".
+                    ci-après l&apos;&ldquo;Editeur&ldquo;.
                 </Text>
-                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 2 - L'HEBERGEUR</Heading>
+                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 2 - L&apos;HEBERGEUR</Heading>
                 <Text mb={8} mt={4}>
-                    L'hébergeur du Site est la société sitehebergeur, dont le siège social est situé au 5 rue de paris  , avec le numéro de téléphone : 0700000000 + adresse mail de contact
+                    L&apos;hébergeur du Site est la société sitehebergeur, dont le siège social est situé au 5 rue de paris  , avec le numéro de téléphone : 0700000000 + adresse mail de contact
                 </Text>
                 <Heading fontFamily={"marianne"} mb={4}>ARTICLE 3 - ACCES AU SITE</Heading>
                 <Text mb={8} mt={4}>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.
 
-                    En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+                    En cas de modification, interruption ou suspension du Site, l&apos;Editeur ne saurait être tenu responsable.
                 </Text>
                 <Heading fontFamily={"marianne"} mb={4}>ARTICLE 4 - COLLECTE DES DONNEES</Heading>
                 <Text mb={8} mt={4}>
-                    Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
+                    Le Site assure à l&apos;Utilisateur une collecte et un traitement d&apos;informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l&apos;informatique, aux fichiers et aux libertés.
 
-                    En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
-                    ·         par mail à l'adresse email contact@nemesis.com
+                    En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l&apos;Utilisateur dispose d&apos;un droit d&apos;accès, de rectification, de suppression et d&apos;opposition de ses données personnelles. L&apos;Utilisateur exerce ce droit :
+                    ·         par mail à l&apos;adresse email contact@nemesis.com
                     ·         par voie postale au 2 rue des revendications ;
                     ·         via un formulaire de contact ;
 
@@ -61,9 +61,9 @@ export default function Mentionslegales() {
 
                     Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site﻿, sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
 
-                    Pour plus d’informations, se reporter aux CGU du site www.siteopensourcem1.fr accessible à la rubrique "CGU"
-                    Pour plus d'informations en matière de protection des données à caractère personnel , se reporter à la Charte en matière de protection des données à caractère personnel du site www.siteopensourcem1.fr accessible à la rubrique "Données personnelles"
-                    Pour plus d'informations en matière de cookies, se reporter à la Charte en matière de cookies du site www.siteopensourcem1.fr accessible à la rubrique "Cookies"
+                    Pour plus d’informations, se reporter aux CGU du site www.siteopensourcem1.fr accessible à la rubrique &ldquo;CGU&ldquo;
+                    Pour plus d&apos;informations en matière de protection des données à caractère personnel , se reporter à la Charte en matière de protection des données à caractère personnel du site www.siteopensourcem1.fr accessible à la rubrique &ldquo;Données personnelles&ldquo;
+                    Pour plus d&apos;informations en matière de cookies, se reporter à la Charte en matière de cookies du site www.siteopensourcem1.fr accessible à la rubrique &ldquo;Cookies&ldquo;
                 </Text>
                 
 
