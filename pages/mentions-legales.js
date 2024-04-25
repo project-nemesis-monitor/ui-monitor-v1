5d286aaecebf42ba3570bc979681f038a4e5d5c8
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-04-25 21:42:52 +0200

    feat: politique

---
+++ pages/mentions-legales.js
@@ -0,0 +1,73 @@
+import {
+    Box,
+    Image,
+    Input,
+    Button,
+    Center,
+    chakra,
+    FormControl,
+    FormLabel,
+    InputGroup,
+    InputLeftElement,
+    Text,
+    InputRightElement,
+    IconButton,
+    useDisclosure,
+    useBreakpointValue,
+    Select,
+    Heading
+} from "@chakra-ui/react";
+
+export default function Mentionslegales() {
+    return (
+        <>
+            <Box fontFamily={"marianne"} color={"white"} p={14}>
+
+                <Text fontFamily={"marianne"} mb={4}>
+                    Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l""Utilisateur", du site www.siteopensourcem1.fr , ci-après le "Site", les présentes mentions légales.
+
+                    La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.
+
+                    Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+
+                </Text>
+
+
+                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 1 - L'EDITEUR</Heading>
+                <Text mb={8} mt={4}>
+                    L’édition et la direction de la publication du Site est assurée par groupe 4 open source, domiciliée 12 rue anatole france, dont le numéro de téléphone est 06000000, et l'adresse e-mail contact@nemesis,com
+
+                    ci-après l'"Editeur".
+                </Text>
+                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 2 - L'HEBERGEUR</Heading>
+                <Text mb={8} mt={4}>
+                    L'hébergeur du Site est la société sitehebergeur, dont le siège social est situé au 5 rue de paris  , avec le numéro de téléphone : 0700000000 + adresse mail de contact
+                </Text>
+                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 3 - ACCES AU SITE</Heading>
+                <Text mb={8} mt={4}>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.
+
+                    En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+                </Text>
+                <Heading fontFamily={"marianne"} mb={4}>ARTICLE 4 - COLLECTE DES DONNEES</Heading>
+                <Text mb={8} mt={4}>
+                    Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
+
+                    En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
+                    ·         par mail à l'adresse email contact@nemesis.com
+                    ·         par voie postale au 2 rue des revendications ;
+                    ·         via un formulaire de contact ;
+
+                    ·         via son espace personnel ;
+
+                    Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site﻿, sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+
+                    Pour plus d’informations, se reporter aux CGU du site www.siteopensourcem1.fr accessible à la rubrique "CGU"
+                    Pour plus d'informations en matière de protection des données à caractère personnel , se reporter à la Charte en matière de protection des données à caractère personnel du site www.siteopensourcem1.fr accessible à la rubrique "Données personnelles"
+                    Pour plus d'informations en matière de cookies, se reporter à la Charte en matière de cookies du site www.siteopensourcem1.fr accessible à la rubrique "Cookies"
+                </Text>
+                
+
+            </Box>
+        </>
+    )
+}
--- pages/politique.js
+++ pages/politique.js
@@ -24,10 +24,10 @@ export default function PolitiquePage() {
             <Box fontFamily={"marianne"} color={"white"} p={14}>
                 <Heading fontFamily={"marianne"} mb={4}>1. Collecte et utilisaton des données :</Heading>
                 <Text fontFamily={"marianne"} mb={4}>
-                    - NEMESIS s'engage à collecter uniquement les données personnelles nécessaires à la
+                    - NEMESIS s&apos;engage à collecter uniquement les données personnelles nécessaires à la
                     fourniture de ses services et à des ﬁns commerciales légitimes.
-                    - Les données collectées peuvent inclure des informations telles que le nom, l'adresse,
-                    l'adresse e-mail, les informations de paiement, etc.
+                    - Les données collectées peuvent inclure des informations telles que le nom, l&apos;adresse,
+                    l&apos;adresse e-mail, les informations de paiement, etc.
                     - Les données des utilisateurs ne seront pas partagées avec des tiers sans leur
                     consentement explicite, sauf si requis par la loi ou pour fournir les services demandés.
 
@@ -39,38 +39,38 @@ export default function PolitiquePage() {
                     - NEMESIS met en œuvre des mesures de sécurité appropriées pour protéger les données
                     personnelles contre tout accès non autorisé, toute divulgation, toute altération ou toute
                     destruction.
-                    - Ces mesures comprennent l'utilisation de pare-feu, de protocoles de cryptage, de
-                    chiﬀrement AES256, de contrôles d'accès stricts et de procédures de sécurité des
+                    - Ces mesures comprennent l&apos;utilisation de pare-feu, de protocoles de cryptage, de
+                    chiﬀrement AES256, de contrôles d&apos;accès stricts et de procédures de sécurité des
                     informations.
                 </Text>
                 <Heading fontFamily={"marianne"} mb={4}>3. Gestion des accès :</Heading>
-                <Text mb={8} mt={4}>- L'accès aux données personnelles est strictement limité aux employés qui en ont besoin
+                <Text mb={8} mt={4}>- L&apos;accès aux données personnelles est strictement limité aux employés qui en ont besoin
                     pour eﬀectuer leur travail.
                     - Tous les employés ayant accès aux données sont formés sur les bonnes pratiques en
-                    matière de sécurité et sont tenus de respecter les politiques de sécurité de l'entreprise.
+                    matière de sécurité et sont tenus de respecter les politiques de sécurité de l&apos;entreprise.
                 </Text>
                 <Heading fontFamily={"marianne"} mb={4}>4. Sécurité des transactions :</Heading>
-                <Text mb={8} mt={4}>- Les transactions eﬀectuées sur les plateformes de NEMESIS sont sécurisées à l'aide de
+                <Text mb={8} mt={4}>- Les transactions eﬀectuées sur les plateformes de NEMESIS sont sécurisées à l&apos;aide de
                     protocoles de cryptage et de technologies de sécurité de pointe.
                     - Les informations de paiement sont traitées de manière sécurisée et conformément aux
-                    normes de l'industrie.
+                    normes de l&apos;industrie.
                 </Text>
                 <Heading fontFamily={"marianne"} mb={4}>5. Gestion des incidents :</Heading>
                 <Text mb={8} mt={4}>
-                    - NEMESIS dispose d'un plan de gestion des incidents en cas de violation de données ou de
+                    - NEMESIS dispose d&apos;un plan de gestion des incidents en cas de violation de données ou de
                     tout autre problème de sécurité.
-                    - En cas de violation de données, NEMESIS s'engage à informer les autorités compétentes
+                    - En cas de violation de données, NEMESIS s&apos;engage à informer les autorités compétentes
                     et les utilisateurs concernés dans les délais prescrits par la loi.
                 </Text>
                 <Heading fontFamily={"marianne"} mb={4}>6. Conﬁdentialité et conformité :</Heading>
                 <Text mb={8} mt={4}>
-                    - NEMESIS respecte les réglementations en matière de protection des données et s'engage
+                    - NEMESIS respecte les réglementations en matière de protection des données et s&apos;engage
                     à protéger la conﬁdentialité des informations de ses utilisateurs.
                     - Nous nous engageons également à respecter les demandes de nos utilisateurs concernant
-                    l'accès, la correction ou la suppression de leurs données personnelles.
-                    CePe politique de sécurité est sujePe à révision périodique pour garantir sa pertinence et
+                    l&apos;accès, la correction ou la suppression de leurs données personnelles.
+                    Cet politique de sécurité est sujet à révision périodique pour garantir sa pertinence et
                     son eﬃcacité. En utilisant les services de NEMESIS, les utilisateurs reconnaissent avoir lu,
-                    compris et accepté cePe politique de sécurité.
+                    compris et accepté cett politique de sécurité.
                 </Text>
 
             </Box>
